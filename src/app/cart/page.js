51b5843a8e48f1d0be50f7309c2eb950ece22e32
Faheem Hassan <faheemh127@gmail.com>
2024-08-13 11:55:24 +0500

    Initial Code Commit

---
+++ src/app/cart/page.js
@@ -0,0 +1,11 @@
+import Cart from "@/app/components/cart";
+export default function CartPage() {
+  return (
+    <div>
+      
+
+        <Cart />
+      
+    </div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Products from "./components/products";
+import Cart from "./components/cart";
+
+export default function Home() {
+  return (
+   <Products />
+  // <Cart />
+  );
+}
